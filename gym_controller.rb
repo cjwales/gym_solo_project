c3d4761bdd302df9fe3f00bf6a731bcb3d1cb54c
cjwales <cjwales@gmail.com>
2019-09-21 11:21:39 +0100

    configured classes path and page

--- gym_controller.rb
+++ gym_controller.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all')
 
 require_relative('./models/member.rb')
+require_relative('./models/gym_class.rb')
 also_reload('./models/*')
 
 get '/' do
@@ -11,4 +12,9 @@ end
 get '/members' do
   @members = Member.all()
   erb(:"members/index")
+end
+
+get '/classes' do
+  @gym_classes = GymClass.all()
+  erb(:"classes/index")
 end
