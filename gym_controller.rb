6b61ec039dc346763bb329b9ff24f4fd2d259de2
cjwales <cjwales@gmail.com>
2019-09-21 15:28:47 +0100

    added comment labels to controller

--- gym_controller.rb
+++ gym_controller.rb
@@ -5,45 +5,45 @@ require_relative('./models/member.rb')
 require_relative('./models/gym_class.rb')
 also_reload('./models/*')
 
-get '/' do
+get '/' do # HOME
   erb(:welcome)
 end
 
-get '/members' do
+get '/members' do # INDEX
   @members = Member.all()
   erb(:"members/index")
 end
 
-get '/members/new' do
+get '/members/new' do # NEW
   erb(:"members/new")
 end
 
-get '/members/:id' do
+get '/members/:id' do # SHOW
   @member = Member.find(params[:id])
   erb(:"members/show")
 end
 
-post '/members' do
+post '/members' do # CREATE
   member = Member.new(params)
   member.save()
   redirect to '/members'
 end
 
-get '/classes' do
+get '/classes' do # INDEX
   @gym_classes = GymClass.all()
   erb(:"classes/index")
 end
 
-get '/classes/new' do
+get '/classes/new' do # NEW
   erb(:"classes/new")
 end
 
-get '/classes/:id' do
+get '/classes/:id' do # SHOW
   @gym_class = GymClass.find(params[:id])
   erb(:"classes/show")
 end
 
-post '/classes' do
+post '/classes' do # CREATE
   gym_class = GymClass.new(params)
   gym_class.save()
   redirect to '/classes'
