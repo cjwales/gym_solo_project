3207f95ed34cff44a00470ad3689ff0f54d10a30
cjwales <cjwales@gmail.com>
2019-09-21 11:54:00 +0100

    added create new member and class functionality

--- gym_controller.rb
+++ gym_controller.rb
@@ -14,7 +14,27 @@ get '/members' do
   erb(:"members/index")
 end
 
+get '/members/new' do
+  erb(:"members/new")
+end
+
+post '/members' do
+  member = Member.new(params)
+  member.save()
+  redirect to '/members'
+end
+
 get '/classes' do
   @gym_classes = GymClass.all()
   erb(:"classes/index")
+end
+
+get '/classes/new' do
+  erb(:"classes/new")
+end
+
+post '/classes' do
+  gym_class = GymClass.new(params)
+  gym_class.save()
+  redirect to '/classes'
 end
